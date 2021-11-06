a42579d72a361df3954018e7a4e2f073894c12f2
andrew <90sidort@gmail.com>
2021-11-06 19:26:52 +0100

    change connection for db seed

--- src/defResolver/mock.defResolver.ts
+++ src/defResolver/mock.defResolver.ts
@@ -1,15 +1,17 @@
-import { getConnection } from "typeorm";
+import { createConnection } from "typeorm";
 
 import { loadFixtures } from "./../../fixtures/loadScript";
 import connectionTestMethods from "../../test/config/testServerMethods";
+import { config } from "../../test/config/testServerConfig";
 
 export const mockDbQuery = async () => {
   try {
     if (process.env.MODE !== "prod") {
-      const connection = getConnection();
+      const connection = await createConnection(config);
       const entities = await connectionTestMethods.getEntities(connection);
       await connectionTestMethods.cleanAll(connection, entities);
       await loadFixtures(connection);
+      await connection.close();
       return true;
     }
     return false;
